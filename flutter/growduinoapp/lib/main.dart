87c0ae8d33299b106e3d24a4493aad49754c9d3e
Rachel Jewell <kju11@txstate.edu>
2024-04-09 14:55:50 -0500

    connected app to firebase

--- flutter/growduinoapp/lib/main.dart
+++ flutter/growduinoapp/lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:growduinoapp/app/growduino_app.dart';
+import 'package:growduinoapp/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+    );
+
   runApp(const GrowduinoApp());
 }
+
+
 
 
 
