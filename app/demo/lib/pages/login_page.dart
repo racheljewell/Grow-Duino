2c1fd393700a467f3b3a5b94e78d8854ffbb261c
Rachel Jewell <kju11@txstate.edu>
2024-04-10 15:04:58 -0500

    moved things around

--- app/demo/lib/pages/login_page.dart
+++ app/demo/lib/pages/login_page.dart
@@ -1,121 +1,170 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:demo/pages/auth_service.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class GrowduinoApp extends StatelessWidget {
+  GrowduinoApp({super.key});
 
-  // This widget is the root of your application.
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      // Navigate to the home page or perform any other actions upon successful sign-in
+    } catch (e) {
+      // Handle sign-in errors
+      print('Sign-in error: $e');
+      // Show error message to the user
+      // You can use Flutter's SnackBar or showDialog for this purpose
+    }
+  }
+
+  // Function to create a text field with custom input decoration
+  Widget buildTextField({
+    required String labelText,
+    required TextStyle labelStyle,
+    required Color borderColor,
+    required double borderWidth,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0, left: 25.0, right: 25.0),
+      child: TextField(
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: borderColor, width: borderWidth),
+          ),
+          labelText: labelText,
+          labelStyle: labelStyle,
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: borderColor),
+            borderRadius: BorderRadius.circular(45.0),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: borderColor, width: borderWidth * 2),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 30, 127, 1)),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Grow Duino'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter+= 2;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      home: Scaffold(
+        body: Stack(
+          children: [
+            const LoginPage(),
+            Center(
+              child: Container(
+                width: 300,
+                height: 650,
+                decoration: BoxDecoration(
+                  color: const Color(0x00d9d9d9).withOpacity(0.5),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 45),
+                    const Text(
+                      'GrowDuino',
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF013f27),
+                      ),
+                    ),
+                    const SizedBox(height: 85),
+                    buildTextField(
+                      labelText: 'Username',
+                      labelStyle: const TextStyle(fontSize: 20, color: Color.fromARGB(255, 1, 63, 39)),
+                      borderColor: const Color.fromARGB(255, 1, 63, 39),
+                      borderWidth: 1.0,
+                    ),
+                    const SizedBox(height: 30),
+                    buildTextField(
+                      labelText: 'Password',
+                      labelStyle: const TextStyle(fontSize: 20, color: Color.fromARGB(255, 1, 63, 39)),
+                      borderColor: const Color.fromARGB(255, 1, 63, 39),
+                      borderWidth: 1.0,
+                    ),
+                    const SizedBox(height: 30),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 1, 63, 39),
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 10,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: TextButton(
+                        style: ButtonStyle(
+                          minimumSize: MaterialStateProperty.all<Size>(
+                            const Size(100, 50),
+                          ),
+                          foregroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 255, 251, 251)),
+                          overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                            (Set<MaterialState> states) {
+                              if (states.contains(MaterialState.hovered)) {
+                                return const Color.fromARGB(255, 255, 251, 251).withOpacity(0.04);
+                              }
+                              if (states.contains(MaterialState.focused) || states.contains(MaterialState.pressed)) {
+                                return const Color.fromARGB(255, 255, 251, 251).withOpacity(0.12);
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        onPressed: () {
+                          // Replace 'email' and 'password' with the actual values entered by the user
+                          signInWithEmailAndPassword('email', 'password');
+                        },
+                        child: const Text(
+                          'Sign In',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  // final String title;
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/assets/img/home_background.png'), // Replace with your provided image path
+            fit: BoxFit.cover, // Adjust as needed
+          ),
+        ),
+      ),
     );
   }
 }
