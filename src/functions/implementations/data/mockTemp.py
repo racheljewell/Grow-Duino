47501dd47a9eb6e5e808fc5b2287e4756984e387
Rachel Jewell <kju11@txstate.edu>
2024-04-14 16:25:01 -0500

    removed secret

--- src/functions/implementations/data/mockTemp.py
+++ src/functions/implementations/data/mockTemp.py
@@ -9,20 +9,7 @@ import datetime
 app = initialize_app()
 
 def pushMockData():
-    google_creds, _ = google.auth.load_credentials_from_dict({
-        "type": "service_account",
-        "project_id": "grow-duino",
-        "private_key_id": "62e114a98c63deafc2ef28cc3e1bda81ab84d65d",
-        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCvcgOc1Ca9wSbI\nixlFGRg8ckx8RDvx/3UcgqMpa53I3QByUlVRMo6M1uqJs6OOHbUf7I2jkSZfLjic\nKcI3VDPS87sq/mq0VfdxLKtGwxQRfwm23cJAjecTlPgGO8cVa1dONEVcKXVoAsUv\nQ1FQBldvdVI04i1dyHlkF6yoCTdytO512EA2Xi9QtVcHb2ZJVZOZiSVwLFkAKnMq\nmcFuj8MUKs3iR+Nj2q2IC3Ds9/H5dmjV1BQlGYM+GFA2+5PQmAkRWRiQWf5yyN+C\njpgtDYfQW73WpNOIP9gq7yXKJR2wLIapKDo0G2JLyWyMH4rbFzB7aiecjlz5KCP/\nU927tAf3AgMBAAECggEADoiv6VO4p+FZEKW+E94DbRJ86Ph2gAXARZRCGi7RLrAc\nMHXgna4SzNkK+E0g4Fq9EL/cb4kZOCsdXNZ0WxGgv20aFSXOLVIUm9QHyYAxaLRw\n8UZHdTVfYXbNFMGD+W+S5AzrFNQizT+zGeC5dJsp4bcpQ1QPTqSwzBln+X2ixPlt\nReScV2A6CsHfXkThp4+Xh+iZOTFyCFiFR5qxm6S1CI0LntgeMpaaHK3eIpSPdaGN\nDMcsyy5VQels3/2/cT9boAo+Rb1+WMsHmSEr2wbwiiV9YC2jgS6L7+DTUiJAsmjR\nUMWhTUd8EcTE6+7jACRklILZ66P2XB+0GWVWPtFvZQKBgQDzCo23DXU5HKLI/D8b\nK5saLdU746E5esxwXxu+gXylboTBZLCTMPUeg0g4o8t8CTF20HdHbVvniAP3qv2U\nhbvn3ofNBcY1HyMxujSzpOZd6+q+BxtgpSy5hMHtHdUQVBnqPB1ON7w5RdDiCgXm\nilfWIV+UBsSmF4c7RkIBA3l6QwKBgQC4zMuncATJOrYCtVzRtWF0Nwf5x5ETzvnK\nQvmpNKq0kWIVV31IXcmRhfkXgxQBT1p8ZUo90Sev7bAsjVIhnF57R8AaNwv8S546\noadjzavb/H9k5Q1icGBnMXz2Xjf5aS9y5gWRAf9sL5wxF15G0FNk9zrnBfUAR+V4\nCTbsOJciPQKBgA3MlplDGVnBx3hT4h1fz3GgEpBQ71F9KGhk7gVWAsa9zKnSVrg0\numYeHBajLiV/vCA729nhWqt9rIP+YGFzamTS3LEGV+eiOfRT1zQv86h3gZ+Cdcp5\n9l0eLYiR/gvsSoBjI2LShUxXK3H6EbWyOyR8Rpn3/GhVw6bY6Vx3XwlTAoGAU1ja\niTv2jKOUL4iIwnRh7b5cNIWTozF4a4blfzEnxKTFbga1lIvoO2AHRllyDl0x3GT6\npQLOOYSkLpPWdR5HZqant6sYFsQsKxl5m4PJfUBWXLUJ4ln4wNILIDrCWBTet52S\n6SHTh4G5mlKFzL5svl7bDBMduvCyR/8v7D5hu6kCgYEAssB8wXaE3HLvdTWCBGAi\nApfJnzJgmfPLG7LbYPZdXxBgeXO6xCWCJ04eiwyqPHHEbv7XwKqU7d4Y7YxJXmFU\nazjUGO8z6vX2KllrZZRSus7B/OQnNVeYrbAz+lGpgY75oBuCFLLDlo/qTDVSjvvS\nmPOj8cYioc1npIqrNxLHJSY=\n-----END PRIVATE KEY-----\n",
-        "client_email": "firebase-adminsdk-dafan@grow-duino.iam.gserviceaccount.com",
-        "client_id": "107524830934348756066",
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-dafan%40grow-duino.iam.gserviceaccount.com",
-        "universe_domain": "googleapis.com"
-        })
-
+   
         # Initialize Firestore client with the loaded credentials
     db = firestore.Client(project='grow-duino', credentials=google_creds)
     if db is not None:
